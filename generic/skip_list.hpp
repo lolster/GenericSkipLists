25e33a29d7aebb2f4653040e274f51cfaddea360
Sushrith Arkal <sushrith.arkal@gmail.com>
2017-04-19 22:39:36 +0530

    FInished skip_list dtor, fixedbug in node_ dtor
    Untested when data is inserted in the list though.

--- generic/skip_list.hpp
+++ generic/skip_list.hpp
@@ -1,11 +1,15 @@
 #ifndef __SKIP_LIST_H__
 #define __SKIP_LIST_H__
 
+#define DEBUG 1
+
 #include <functional>
 #include <memory>
 #include <limits>
-// #include <iostream>
 
+#if DEBUG
+#include <iostream>
+#endif
 /*
 	See: http://www.mathcs.emory.edu/~cheung/Courses/323/Syllabus/Map/FIGS/skip-list22.gif
 */
@@ -15,30 +19,6 @@ template <
 	typename Compare = std::less<T>,
 	typename Allocator = std::allocator<T>
 > class skip_list {
-private:
-	const size_type h_; // max height of the skip list - doesn't change for now
-	size_type n_; // counter for number of nodes in the list
-	Compare comp = Compare();
-	Allocator alloc = Allocator();
-	struct node_ {
-		T data_;
-		node_ *next_;
-		node_ *prev_;
-		node_ *top_;
-		node_ *bottom_;
-		explicit node_(T data = 0, node_ *next = nullptr, node_ *prev = nullptr, 
-			node_ *top = nullptr, node_ *bottom = nullptr)
-		: data_(data), next_(next), prev_(prev), top_(top), bottom_(bottom)
-		{}
-		~node_() {
-			delete next;
-			delete prev;
-			delete top;
-			delete bottom;
-		}
-	};
-	node_ *head;
-	node_ *tail; //exclusive
 public:
 	//Member types
 	using value_type       =  T;
@@ -46,8 +26,8 @@ public:
 	using size_type        =  std::size_t;
 	using reference        =  value_type&;
 	using const_reference  =  const value_type&;
-	using pointer          =  std::allocator_traits<allocator_type>::pointer;
-	using const_pointer    =  std::allocator_traits<allocator_type>::const_pointer;
+	using pointer          =  typename std::allocator_traits<allocator_type>::pointer;
+	using const_pointer    =  typename std::allocator_traits<allocator_type>::const_pointer;
 	//TODO
 	// using difference_type = 
 	// using iterator = 
@@ -55,23 +35,95 @@ public:
 	// using reverse_iterator = 
 	// using const_reverse_iterator = 
 
+private:
+	const size_type h_; // max height of the skip list - doesn't change for now
+	size_type n_; // counter for number of nodes in the list
+	Compare comp = Compare();
+	Allocator alloc = Allocator();
+	struct node_ {
+		using node_type_t = long;
+		
+		//class consts
+		static const node_type_t HEAD_NODE = 1;
+		static const node_type_t DATA_NODE = 2;
+		static const node_type_t TAIL_NODE = 3;
 
-	skip_list(size_type h = 1)
-	: h_(h), n_(0)
-	{
-		//TODO
-		tail = new node_();
-		head = tail;
-	}
-	~skip_list() {
-		//TODO
-		while(head != tail) {
-			delete head++;
+
+		value_type data_;
+		const node_type_t type_;
+		node_ *next_;
+		node_ *prev_;
+		node_ *top_;
+		node_ *bottom_;
+		explicit node_(value_type data = 0, node_type_t type = DATA_NODE, node_ *next = nullptr, node_ *prev = nullptr, 
+			node_ *top = nullptr, node_ *bottom = nullptr)
+		: data_(data), next_(next), prev_(prev), top_(top), bottom_(bottom), type_(type)
+		{}
+
+		~node_() {
+			// i am such an idiot
+			// delete next_;
+			// delete prev_;
+			// delete top_;
+			// delete bottom_;
+			// std::cout << "Delete me\n";
+			// No RA, therefore no RD
 		}
-		delete head;
+	};
+
+	//pointers to head and tail
+	node_ *head_;
+	node_ *tail_; //exclusive
+
+public:
+	skip_list(size_type h = 1)
+	: h_(h), n_(0) {
+		// TODO
+		// make a stack of head and tail nodes about ye (h_) high
+		// please work
+		head_ = new node_(0, node_::HEAD_NODE);
+		tail_ = new node_(0, node_::TAIL_NODE);
+		head_->next_ = tail_;
+		tail_->prev_ = head_;
+		node_ *h_t = head_;
+		node_ *t_t = tail_;
+		for(size_type i = 1; i < h_; ++i) {
+			// TODO - make more efficient
+			h_t->bottom_ = new node_(0, node_::HEAD_NODE);
+			h_t->bottom_->top_ = h_t;
+			t_t -> bottom_ = new node_(0, node_::TAIL_NODE);
+			t_t->bottom_->top_ = t_t;
+			h_t->bottom_->next_ = t_t->bottom_;
+			t_t->bottom_->prev_ = h_t->bottom_;
+			h_t = h_t->bottom_;
+			t_t = t_t->bottom_;
+		}
 	}
 
-	//member functions
+	~skip_list() {
+		// Logic:
+		// Iterate through each level and delete the nodes across the level
+		for(size_type i = 0; i < h_; ++i) {
+			node_ *next_level = head_->bottom_;
+			while(head_) {
+				node_ *temp = head_;
+				head_ = head_->next_;
+				delete temp;
+				std::cout << "one\n";
+			}
+			head_ = next_level;
+		}
+	}
+
+	// member functions
+	// TODO
+	// operator=
+	// assign
+
+	allocator_type get_allocator() {
+		return alloc;
+	}
+
 	//capacity
 	bool empty() {
 		return n_ == 0;
@@ -89,7 +141,7 @@ public:
 	//TODO
 	class Iterator {
 	public:
-	}
+	};
 };
 
 #endif

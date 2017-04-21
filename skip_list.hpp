77693dd722850c2e109fcdd6558e433c397f8074
Sushrith Arkal <sushrith.arkal@gmail.com>
2017-04-21 23:28:51 +0530

    fixed node operator<  for pointer values

--- skip_list.hpp
+++ skip_list.hpp
@@ -81,12 +81,12 @@ private:
 			return this->next == nullptr && this->data == nullptr;
 		}
 
-		friend bool operator<(node& lhs, node& rhs) {
-			if(lhs.is_head())
+		friend bool operator<(node* lhs, node* rhs) {
+			if(lhs->is_head())
 				return true;
-			if(rhs.is_tail())
+			if(rhs->is_tail())
 				return false;
-			return *(lhs.data) < *(rhs.data);
+			return comp(*(lhs->data),*(rhs->data));
 		}
 	};
 
